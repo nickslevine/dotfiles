73ae02f9c4d9743a179c75d2d78d39dc06849425
Nick Levine <nickslevine@gmail.com>
2025-10-03 17:40:34 +0000

    tmux conf

--- setup-hyperbolic.sh
+++ setup-hyperbolic.sh
@@ -24,13 +24,18 @@ npm install -g @anthropic-ai/claude-code
 sudo apt-get install -y ripgrep 
 
 ## Helix
-tmp_dir="$(mktemp -d)" || return 1
-deb_file="${tmp_dir}/helix.deb"
-sudo apt install -y "${deb_file}"
+sudo add-apt-repository ppa:maveonair/helix-editor
+sudo apt update
+sudo apt install helix
 
-## Zellij
 
-brew install zellij
+### Eza
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
 
 ## Starship
 curl -sS https://starship.rs/install.sh | sudo sh
@@ -43,3 +48,9 @@ echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/ubuntu/.b
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 source ~/.bashrc
+
+## Zellij
+
+brew install zellij
+
+
